c92c65c24afc9b464e2b06a60b915e2737ff64d8
rkhapov <r.khapov@yandex.ru>
2018-09-15 01:29:14 +0500

    delete some shit

---
+++ protocol/ftp.py
@@ -0,0 +1,18 @@
+from network.tcp import TcpConnection
+from protocol.replies_reader import RepliesReader
+
+
+class FtpClient:
+    def __init__(self, tcp_connection: TcpConnection):
+        self.__tcp_connection = tcp_connection
+        self.__replies_reader = RepliesReader(self.__tcp_connection)
+
+    @property
+    def tcp_connection(self):
+        return self.__tcp_connection
+
+    @property
+    def replies_reader(self):
+        return self.__replies_reader
+
+
