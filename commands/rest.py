20d9e8a745e457ffc1746838a6a8f41e957fc691
rkhapov <r.khapov@yandex.ru>
2018-10-21 22:45:49 +0500

    fix strane order

--- commands/rest.py
+++ commands/rest.py
@@ -36,16 +36,16 @@ class RestCommand(Command):
         return 'restore filename $outfilename'
 
     def _execute_port(self, client):
-        offset = self._get_offset()
         filename = self.get_argument('filename')
 
+        size = self._get_size(client, filename)
+
+        offset = self._get_offset()
         if offset is None:
             print('Cant get offset to download')
             return
 
-        size = self._get_size(client, filename)
-
-        if offset >= size:
+        if size is not None and offset >= size:
             print('No downloading needed')
             return
 
